63c81f637b3827fb8ca605375ba6696bab3abf1b
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-11-21 14:50:15 -0500

    Add shoot cooldown

--- project/src/Gamestats.gd
+++ project/src/Gamestats.gd
@@ -3,6 +3,8 @@ extends Node
 var health := 5
 var score := 0
 
+var player : Player 
+
 var ship_texture : Texture = preload("res://assets/images/hero-ships/009.png")
 
 func _ready():
--- project/src/Player.gd
+++ project/src/Player.gd
@@ -4,13 +4,18 @@ extends KinematicBody2D
 signal destroyed
 
 export var speed := 200
+export var cooldown := 0.4
+
+var _can_shoot := true
 
 onready var _sprite := $Sprite
 onready var _shoot_sound := $ShootSound
 onready var _hit_sound := $HitSound
+onready var _shoot_cooldown := $ShootCooldown
 
 func _ready():
 	_sprite.texture = Gamestats.ship_texture
+	Gamestats.player = self
 	
 
 func _physics_process(delta):
@@ -19,13 +24,20 @@ func _physics_process(delta):
 		velocity -= 1
 	if Input.is_action_pressed("Right"):
 		velocity += 1
-	if Input.is_action_just_pressed("Shoot"):
+	if Input.is_action_just_pressed("Shoot") and _can_shoot:
+		_can_shoot = false
+		_shoot_cooldown.wait_time = cooldown
+		_shoot_cooldown.start()
 		_shoot_sound.play()
 		var Laser:Area2D = load("res://src/Laser.tscn").instance()
 		Laser.good = true
 		Laser.global_position = $Muzzle.get_global_transform().origin
 		get_parent().add_child(Laser)
 	var _error = move_and_collide(Vector2(velocity, 0)*delta*speed)
+
+
+func can_shoot()->bool:
+	return _can_shoot
 
 
 func hit():
@@ -46,3 +58,7 @@ func hit():
 
 func _set_ship_variant(value:int)->void:
 	_sprite.frame = value
+
+
+func _on_ShootCooldown_timeout():
+	_can_shoot = true
