514b533e653cbd830b26bbd344e20120bb97030c
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-11-21 10:16:39 -0500

    Fix wave not printing message when all enemies are dead

--- project/src/Wave.gd
+++ project/src/Wave.gd
@@ -8,17 +8,20 @@ export var Enemy : PackedScene = preload("res://src/Enemy.tscn")
 
 onready var _spawn_points := $SpawnPoints
 onready var _enemies := $Enemies
+var _enemies_left := 0
 
 func _ready():
 	for spawn_point in _spawn_points.get_children():
 		var enemy : Enemy = Enemy.instance()
 		enemy.position = spawn_point.position
 		_enemies.add_child(enemy)
+		_enemies_left += 1
 		var _ignored := enemy.connect("destroyed", self, "_on_Enemy_destroyed", [], CONNECT_ONESHOT)
 		
 
 func _on_Enemy_destroyed():
-	if _enemies.get_child_count() == 0:
+	_enemies_left -= 1
+	if _enemies_left == 0:
 		print("All enemies in this wave are destroyed")
 		emit_signal("completed")
 
